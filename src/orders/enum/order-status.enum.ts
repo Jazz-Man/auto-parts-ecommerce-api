82ef4ec61d42f1df66b327a95cdb81ceed0c44f7
Vasyl Sokolyk <vsokolyk@gmail.com>
2026-05-13 19:52:39 +0300

    feat(orders): add OrderStatus enum

---
+++ src/orders/enum/order-status.enum.ts
@@ -0,0 +1,8 @@
+// src/orders/enum/order-status.enum.ts
+export enum OrderStatus {
+  Pending = 'pending',
+  Paid = 'paid',
+  Shipped = 'shipped',
+  Delivered = 'delivered',
+  Cancelled = 'cancelled',
+}
